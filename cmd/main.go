6cb620c4e936e16c9a632608913a1634742c703e
mehmet.aran <mehmet.aran@trendyol.com>
2021-04-29 22:22:42 +0300

    Temp api is created without any db

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,73 @@
 package main
 
 import (
-	"fmt"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"net/http"
+	"strconv"
+	)
+
+type (
+	user struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	}
 )
 
+var (
+	users = map[int]*user{}
+	seq   = 1
+)
+
+
+func createUser(c echo.Context) error {
+	u := &user{
+		ID: seq,
+	}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	users[u.ID] = u
+	seq++
+	return c.JSON(http.StatusCreated, u)
+}
+
+func getUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func updateUser(c echo.Context) error {
+	u := new(user)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	users[id].Name = u.Name
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func deleteUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(users, id)
+	return c.NoContent(http.StatusNoContent)
+}
+
+func getAllUsers(c echo.Context) error {
+	return c.JSON(http.StatusOK, users)
+}
+
 func main() {
-	fmt.Println("hello world")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/users", getAllUsers)
+	e.POST("/users", createUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
